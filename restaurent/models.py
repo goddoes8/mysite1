311db61500f7116121097fbc03ee6611ae8bdefc
raegold <goddoes8@gmail.com>
2024-05-06 18:11:05 +0900

    first commit

---
+++ restaurent/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Restaurent(models.Model):
+    # 이름
+    name = models.CharField(max_length=50)
+    # 주소
+    address = models.CharField(max_length=255)
+    # 전화번호
+    phone = models.CharField(max_length=20)
+    # 위도
+    latitude = models.DecimalField(max_dights=9, decimal_places=6)
+    # 경도
+    longitude = models.DecimalField(max_dights=9, decimal_places=6)
+    # 점수
+    rating = models.SmallIntegerField()
+    # 생성 시간
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 업데이트 시간
+    updated_at = models.DateTimeField(auto_now=True)
